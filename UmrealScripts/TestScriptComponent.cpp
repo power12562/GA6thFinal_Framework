2397b7dd63a7075915846eea2af76597d1540371
power12562 <kimsiwoo1005@gmail.com>
2025-03-15 20:01:32 +0900

    commit
    commit

--- UmrealScripts/TestScriptComponent.cpp
+++ UmrealScripts/TestScriptComponent.cpp
@@ -9,6 +9,6 @@ void TestComponent::Update()
 {
     //엔진 전역 접근 어케함???????
     
-    //ImGui::Text("테스트 컴포넌트 업데이트 호출중임");
-    //float dt = Time.deltaTime();
+    //ImGui::Text((const char*)u8"테스트 컴포넌트 업데이트 호출중임");
+    float dt = Time.deltaTime();
 }
